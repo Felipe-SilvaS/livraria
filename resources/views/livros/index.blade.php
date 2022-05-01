6b99f1b05334ab5923c54afd32cafafaa53a3fb7
Felipe Silva <felipe.silvads@outlook.com>
2022-04-30 23:13:23 -0300

    Criado funcao index no LivroController e Adicionado listagem de livros no livros/index.blade.php

--- resources/views/livros/index.blade.php
+++ resources/views/livros/index.blade.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Livros</title>
 </head>
+
 <body>
     <div>
         <h2>Lista de Livros</h2>
         @foreach ($livros as $livro)
-            
+            <p>Título: {{ $livro->titulo }}</p>
+            <p>Ano: {{ $livro->ano }}</p>
+            <p>Idioma: {{ $livro->idioma }}</p>
+            <p>ISBN: {{ $livro->isbn }}</p>
+            <hr>
         @endforeach
     </div>
 </body>
+
 </html>
