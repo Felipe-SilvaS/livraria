55143bc6492e2ab6c948d8332892910e8fe992af
Felipe Silva <felipe.silvads@outlook.com>
2022-05-11 23:06:30 -0300

    Atualização do layout base

--- resources/views/autors/index.blade.php
+++ resources/views/autors/index.blade.php
@@ -18,8 +18,85 @@
                 <a href="{{ route('autors.show', $autors->id) }}">[detalhes]</a>
                 <a href="{{ route('autors.edit', $autors->id) }}">[editar]</a>
             </section>
-
             <hr>
         @endforeach
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ad praesentium pariatur cupiditate quo ipsum
+            fugit minus, recusandae quisquam culpa ex numquam qui iste repellat similique nulla! Error, magni a?</p>
+
+
+
+
     </section>
 @endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,15 +1,79 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+
     <title>Livraria</title>
 </head>
 
 <body>
-    <!-- conteudo da pagina -->
-    @yield('content')
+    <div class="d-flex" id="wrapper">
+        <!-- nav bar -->
+        <nav class="d-flex flex-column p-3 text-white bg-dark">
+
+            <div class="d-flex align-items-center mb-3 mb-md-0 me-md-auto mx-auto text-white">
+                <i class="fa-solid fa-book-open fa-lg me-2"></i>
+                <span class="fs-4">Livraria</span>
+            </div>
+            <hr>
+
+            <ul class="navbar-nav nav-pills flex-column mb-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link active" aria-current="page">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#home" />
+                        </svg>
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#speedometer2" />
+                        </svg>
+                        Dashboard
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#table" />
+                        </svg>
+                        Orders
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#grid" />
+                        </svg>
+                        Products
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#people-circle" />
+                        </svg>
+                        Customers
+                    </a>
+                </li>
+            </ul>
+            <hr>
+        </nav>
+        <!-- conteudo da pagina -->
+        <main class="p-3" id="content">
+            @yield('content')
+        </main>
+    </div>
 </body>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
 </html>
